1fcb8221f63d86526189c7d1ace897fcf3c27843
abein <54259142+abe-kazuki@users.noreply.github.com>
2020-04-20 16:45:14 +0900

    Welcome to AtCoder.playground
    実行時間の短縮をしました。

--- Welcome to AtCoder.playground/Contents.swift
+++ Welcome to AtCoder.playground/Contents.swift
@@ -1,13 +1,11 @@
 import Foundation
-
+ 
 let a: Int = Int(readLine()!) ?? 1
-
-let  bc = ( readLine()!).components(separatedBy: " ")
-let b =  Int(bc[0]) ?? 1
-let c =  Int(bc[1]) ?? 1
-
-let sum = a+b+c
-
+ 
+let bc = ( readLine()!).components(separatedBy: " ")
+ 
+let sum = a+(Int(bc[0]) ?? 1)+(Int(bc[1]) ?? 1)
+ 
 let str = readLine()!
-
+ 
 print(sum,str)
