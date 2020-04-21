ae361e6f771c31a4ace2070e70cfc7a9df1ce757
abein <54259142+abe-kazuki@users.noreply.github.com>
2020-04-21 14:00:08 +0900

    三項演算子にて出力条件を記述
    三項演算子を用いてコードを短縮

--- Product.playground/Contents.swift
+++ Product.playground/Contents.swift
@@ -4,12 +4,7 @@ let  numbers = readLine()!
 if numbers.count > 1{
     let numbers_array =  numbers.components(separatedBy: " ")
 
-     let sum = (Int(numbers_array[0]) ?? 1) * (Int(numbers_array[1]) ?? 1)
+    let sum = (Int(numbers_array[0]) ?? 1) * (Int(numbers_array[1]) ?? 1)
 
-    if sum %  2  == 0{
-          print("Even")
-    }
-    else{
-         print("Odd")
-   }
+    print(sum %  2  == 0 ? "Even" : "Odd")
 }
