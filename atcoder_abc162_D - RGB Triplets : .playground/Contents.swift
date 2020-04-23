f195a5c89638539c6b7fb141b631b8db668738bd
abein <54259142+abe-kazuki@users.noreply.github.com>
2020-04-23 14:16:05 +0900

    テストが全て通る様に修正しました。
    atcoder_abc162_D - RGB Triplets / .playgroundの処理時間を規定内でクリアできたソースです。

--- atcoder_abc162_D - RGB Triplets : .playground/Contents.swift
+++ atcoder_abc162_D - RGB Triplets : .playground/Contents.swift
@@ -1,31 +1,33 @@
-let K = 4
-let rgbArray = Array("RRGB")
+let N = Int(readLine()!) ?? 0
+let rgbArray = Array(readLine()!)
 var count = 0
-
 var rgbIndex = Array(repeating: Array(repeating: 0, count: 0), count: 3)
-
+ 
 rgbArray.enumerated().forEach{
     if $0.1 == "R"{
         rgbIndex[0].append($0.0)
     }
-    else if $0.1 == "G"{
+    if $0.1 == "G"{
         rgbIndex[1].append($0.0)
     }
-    else{
+    if $0.1 == "B"{
         rgbIndex[2].append($0.0)
     }
+ 
+    
 }
-print(rgbIndex)
-for i in rgbIndex[0]{
-    for j in rgbIndex[1]{
-            for k in rgbIndex[2]{
-                if (j - i !=  k - j) && (i < j && j < k){
-                    print(i,j,k)
-                    count += 1
-                      
-                }
-            }
+var total = rgbIndex[2].count*rgbIndex[1].count*rgbIndex[0].count
+ 
+for i in 0 ..< N {
+    for j in i+1 ..< N {
+        let k = 2 * j - i
+        if k >= N {
+          break
+        }
+        if rgbArray[i] != rgbArray[j] && rgbArray[j] != rgbArray[k] && rgbArray[k] != rgbArray[i] {
+            total -= 1
+        }
     }
 }
-print(count)
-print(rgbIndex[0].count*rgbIndex[1].count*rgbIndex[2].count)
+ 
+print(total)
