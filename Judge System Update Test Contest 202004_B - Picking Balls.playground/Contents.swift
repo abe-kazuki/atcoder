646666b01539dae698efa4b5f575fcfbf8a52f30
abein <54259142+abe-kazuki@users.noreply.github.com>
2020-04-23 16:54:30 +0900

    Judge System Update Test Contest 202004_B
    Judge System Update Test Contest 202004の問題「B - Picking Balls.playground」に提出したソースです。
    ↓
    問題：https://atcoder.jp/contests/judge-update-202004/tasks/judge_update_202004_b

---
+++ Judge System Update Test Contest 202004_B - Picking Balls.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+let num = Int(readLine()!) ?? 0
+var array: [[Int]] = Array(repeating: Array(repeating: 0, count: 0), count: 2)
+for _ in 1...num{
+    var input = (readLine()!).components(separatedBy: " ")
+    if input[1]  == "R"{
+      array[0].append(Int(input[0]) ??  0)
+    }
+      else{
+      array[1].append(Int(input[0]) ??  0)
+    }
+}
+for var i in array{
+    i.sort{$0 < $1}
+    i.forEach{
+      print($0)
+    }
+}
